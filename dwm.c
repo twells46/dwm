2ebc59b7e3e2b3b78f45edc2ac95a81347173604
twells <twells@aurthur.com>
2024-06-07 21:56:14 -0500

    Patch with hide vacant tags

--- dwm.c
+++ dwm.c
@@ -455,9 +455,17 @@ buttonpress(XEvent *e)
 	if (ev->window == selmon->barwin) {
                 if (ev->x < ble - blw) {
                         i = -1, x = -ev->x;
-                        do
+                        do {
                                 x += TEXTW(tags[++i]);
-                        while (x <= 0);
+                        	unsigned int occ = 0;
+                                for(c = m->clients; c; c=c->next)
+                                        occ |= c->tags == TAGMASK ? 0 : c->tags;
+                                do {
+                                        /* Do not reserve space for vacant tags */
+                                        if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+                                                continue;
+		                } while (ev->x >= x && ++i < LENGTH(tags));
+                        } while (x <= 0);
                         click = ClkTagBar;
                         arg.ui = 1 << i;
                 } else if (ev->x < ble)
@@ -763,19 +771,18 @@ drawbar(Monitor *m)
 	}
 
 	for (c = m->clients; c; c = c->next) {
-		occ |= c->tags;
+		occ |= c->tags == TAGMASK ? 0 : c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
+		/* Do not draw vacant tags */
+		if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+			continue;
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-				urg & 1 << i);
 		x += w;
 	}
 	w = TEXTW(m->ltsymbol);
